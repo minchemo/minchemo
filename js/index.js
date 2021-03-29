ffdcc06bbdd7ddbbd861bc70335dd1e626f220a0
lalab996 <lalab996@hotmail.com.tw>
2021-03-29 13:46:01 +0800

    0329

--- js/index.js
+++ js/index.js
@@ -1,18 +1,25 @@
-$( document ).ready(function() {
-    $('.work').click(function(){
+$(document).ready(function () {
+    $('.work').click(function () {
         $('.popup').css({
-            'height': '100vh',
-            'display': 'block'
-        })
-    })
-    $('.popup-close').click(function(){
+            height: '100vh',
+            display: 'block',
+        });
+    });
+    $('.popup-close').click(function () {
         $('.popup').css({
-            'height': '0vh',
-            'display': 'none'
-        })
-    })
+            height: '0vh',
+            display: 'none',
+        });
+    });
+    $('a').click(function (e) {
+        e.preventDefault();
+
+        if ($(this).attr('href')) {
+            window.open($(this).attr('href'), '_blank');
+        }
+    });
 });
 
-function openLink(url){
-    window.open(url,"_blank")
+function openLink(url) {
+    window.open(url, '_blank');
 }
